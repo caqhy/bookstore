527aec705bd999dfed1c7432f8e4a56e18d9de7b
caqhy <1809309636@qq.com>
2021-01-21 23:06:53 +0800

    添加代码

--- bookstore/src/com/caqhy/dao/UserDao.java
+++ bookstore/src/com/caqhy/dao/UserDao.java
@@ -1,16 +1,16 @@
 package com.caqhy.dao;
 
+
 import com.caqhy.pojo.User;
 
-import java.sql.Connection;
 
 public interface UserDao {
 
-    User queryUserByUsername(Connection conn, String username);
+    User queryUserByUsername(String username);
 
 
-    User queryUserByUsernameAndPassword(Connection conn, String username, String password);
+    User queryUserByUsernameAndPassword( String username, String password);
 
 
-    int saveUser(Connection conn, User user);
+    int saveUser( User user);
 }
