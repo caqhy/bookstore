1ab064cba92715973d3f07e55c408999e8ef0a62
caqhy <1809309636@qq.com>
2021-01-22 22:20:34 +0800

    修改Dao

--- bookstore/src/com/caqhy/dao/OrderDao.java
+++ bookstore/src/com/caqhy/dao/OrderDao.java
@@ -1,4 +1,9 @@
 package com.caqhy.dao;
 
+import com.caqhy.pojo.Order;
+
 public interface OrderDao {
+
+    public int saveOrder(Order order);
+
 }
--- bookstore/src/com/caqhy/dao/OrderItemDao.java
+++ bookstore/src/com/caqhy/dao/OrderItemDao.java
@@ -1,4 +1,9 @@
 package com.caqhy.dao;
 
+import com.caqhy.pojo.OrderItem;
+
 public interface OrderItemDao {
+
+    public int saveOrderItem(OrderItem orderItem);
+
 }
